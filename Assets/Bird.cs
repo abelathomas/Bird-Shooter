8acf72c2cc613794ee9a8f26da6f02531bad80fc
Abel Thomas <abelathomas@gmail.com>
2020-06-30 15:55:57 -0700

    base game

---
+++ Assets/Bird.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
+
+public class Bird : MonoBehaviour
+{
+    private Vector3 _initialPosition;
+    private bool _birdWasLaunched = false;
+    private float _timeSittingAround;
+
+    [SerializeField] private float _launchPower = 250;
+
+    private void Awake()
+    {
+        _initialPosition = transform.position;
+    }
+
+    private void Update()
+    {
+        GetComponent<LineRenderer>().SetPosition(0, transform.position);
+        GetComponent<LineRenderer>().SetPosition(1, _initialPosition);
+
+        if (_birdWasLaunched && GetComponent<Rigidbody2D>().velocity.magnitude <= 0.1)
+        {
+            _timeSittingAround += Time.deltaTime;
+        }
+
+        if (transform.position.y > 10 || transform.position.y < -10 || transform.position.x > 10 || transform.position.x < -20 || _timeSittingAround > 1.5)
+        {
+            string currentSceneName = SceneManager.GetActiveScene().name;
+            SceneManager.LoadScene(currentSceneName);
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        GetComponent<SpriteRenderer>().color = Color.red;
+        GetComponent<LineRenderer>().enabled = true;
+    }
+
+    private void OnMouseUp()
+    {
+        GetComponent<SpriteRenderer>().color = Color.white;
+
+        Vector2 directionToInitialPosition = _initialPosition - transform.position;
+        GetComponent<Rigidbody2D>().AddForce(directionToInitialPosition * _launchPower);
+        GetComponent<Rigidbody2D>().gravityScale = 1;
+        _birdWasLaunched = true;
+
+        GetComponent<LineRenderer>().enabled = false;
+    }
+
+    private void OnMouseDrag()
+    {
+        Vector3 newposition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = new Vector3(newposition.x, newposition.y) ;
+    }
+}
---
+++ Assets/Enemy.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private GameObject _poofParticlePrefab;
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Bird bird = collision.collider.GetComponent<Bird>();
+        if ( bird == true)
+        {
+            Instantiate(_poofParticlePrefab,transform.position,Quaternion.identity);
+            Destroy(gameObject);
+            return;
+        }
+
+        Enemy enemy = collision.collider.GetComponent<Enemy>();
+        if (enemy == true )
+        {
+            return;
+        }
+
+        if ( collision.contacts[0].normal.y < -0.5 )
+        {
+            Instantiate(_poofParticlePrefab, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+            return;
+        }
+    }
+}
